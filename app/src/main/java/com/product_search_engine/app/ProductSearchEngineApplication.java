199a685dff2cbde8f723bef7886078d7a562f56d
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-03-10 16:58:57 +0530

    Backend setup

---
+++ app/src/main/java/com/product_search_engine/app/ProductSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.product_search_engine.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSearchEngineApplication.class, args);
+	}
+
+}
